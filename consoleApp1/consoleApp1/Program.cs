bee3d49bb7d11f3975d3243a939531bbff76b9b6
foxwarriors <adi_ciuca@yahoo.com>
2022-02-01 23:08:18 +0200

    Added new project files - first commit

---
+++ consoleApp1/consoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
