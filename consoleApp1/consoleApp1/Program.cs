03be9aece8b0a2518c210086e41850676e64035c
foxwarriors <adi_ciuca@yahoo.com>
2022-02-02 00:10:45 +0200

    added message

--- consoleApp1/consoleApp1/Program.cs
+++ consoleApp1/consoleApp1/Program.cs
@@ -7,6 +7,8 @@ namespace consoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!!!");
+            Console.WriteLine("my branch");
+
         }
     }
 }
