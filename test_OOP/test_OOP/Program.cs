95ecaddd731f1bbe2e1734cfa2a4707a47f7d9c7
foxwarriors <adi_ciuca@yahoo.com>
2022-02-05 17:46:06 +0200

    added oop inheritance example

---
+++ test_OOP/test_OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace test_OOP
+{
+    public class person
+    {
+        public string name;
+        public string IDcard;
+    }
+        public class student: person
+        {
+            public string domeniu;
+            public int[] note;
+        }
+        
+    class Program
+    {
+        static void Main()
+        {
+            student x = new student();
+            x.name = "viorel";
+            x.IDcard = "vx69";
+            x.domeniu = "IT";
+
+            Console.WriteLine(x.name + " " + x.IDcard + " " + x.domeniu);
+        }
+    }
+}
