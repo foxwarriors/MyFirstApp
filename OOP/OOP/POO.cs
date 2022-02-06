5a099259832017920f7ff45330ae3c6ac5e4263a
foxwarriors <adi_ciuca@yahoo.com>
2022-02-06 16:45:18 +0200

    more OOP options

--- OOP/OOP/POO.cs
+++ OOP/OOP/POO.cs
@@ -3,13 +3,50 @@ namespace OOP
 {
     class Car
     {
-        public string Brand;
+        private string Brand;
 
-        public string Model;
+        private string Model;
 
-        public int AnConstructie;
+        private int AnConstructie;
 
-        public string Culoare;
+        private string Culoare;
+
+        public Car()
+        {
+            Brand = "-";
+            Model = "-";
+            AnConstructie = 2020;
+            Culoare = "Gri";
+
+        }
+
+        public Car(string brand, string model, int anct, string culoare)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.AnConstructie = anct;
+            this.Culoare = culoare;
+        }
+
+        public string getCuloare()
+        {
+            return Culoare;
+        }
+
+        public string getBrand()
+        {
+            return Brand;
+        }
+
+        public int getAn()
+        {
+            return AnConstructie;
+        }
+
+        public string getModel()
+        {
+            return Model;
+        }
 
         public void Start()
         {
@@ -26,4 +63,137 @@ namespace OOP
             Console.WriteLine("Braked!");
         }
     }
+
+    public class Person
+    {
+        protected string Name;
+
+        protected string ID;
+    }
+
+    public class Student : Person
+    {
+
+        public Student(string name, string id)
+        {
+            this.Name = name;
+            this.ID = id;
+        }
+        
+        public string Domain;
+
+        public int AnAbsolvire;
+
+        public int[] Note;
+
+        public void prezent()
+        {
+            Console.WriteLine($"Buna, ma numesc {this.Name}, ID :{this.ID}, sunt student la {this.Domain}");
+        }
+
+    }
+
+    public class BankAccount
+    {
+        private double balance;
+
+        public double Balance
+        {
+            get { return balance; }
+        }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine("Your balance is " + Balance);
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            balance -= amount;
+        }
+
+    }
+
+    public abstract class Animal
+    {
+        public abstract void MakeSound();
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+
+    public abstract class Shape
+    {
+
+        public abstract double Area();
+    }
+
+    public class Rectangle : Shape
+    {
+        public double l;
+        public double L;
+
+        public override double Area()
+        {
+            return L * l;
+
+        }
+    }
+    public class Circle : Shape
+    {
+        public double pi = 3.14;
+        public double r;
+
+        public override double Area()
+        {
+            return pi * r * r;
+
+        }
+
+    }
+
+
+    public class CalculatorSalariu
+    {
+        public static int NumarLuni = 12;
+
+        public double SalariuAnual(double salariuLunar)
+        {
+            return salariuLunar * NumarLuni;
+
+        }
+    }
+
 }
--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -7,17 +7,17 @@ namespace OOP
         static void Main(string[] args)
         {
 
-            Car x = new Car();
-            x.AnConstructie = 2018;
+            Car x = new Car("Mercedes", "S Klasse", 2020, "Rosu");
+           /* x.AnConstructie = 2018;
             x.Brand = "Mercedes";
             x.Culoare = "Rosu";
-            x.Model = "S Klasse";
+            x.Model = "S Klasse";*/
 
-            Car x1 = new Car();
-            x1.AnConstructie = 2020;
+            Car x1 = new Car("BMW","X6", 2021, "Negru");
+            /*x1.AnConstructie = 2020;
             x1.Brand = "BMW";
             x1.Culoare = "Negru";
-            x1.Model = "X6";
+            x1.Model = "X6";*/
 
             x.Start();
             x1.Start();
@@ -27,8 +27,44 @@ namespace OOP
 
             x1.Stop();
 
-            Console.WriteLine($"The car is a {x.Brand} {x.Model} and it was built in {x.AnConstructie}");
-            Console.WriteLine($"The car is a {x1.Brand} {x1.Model} and it was built in {x1.AnConstructie}");
+            Console.WriteLine($"The car is a {x.getBrand()} {x.getModel()} and it was built in {x.getAn()}");
+            Console.WriteLine($"The car is a {x1.getBrand()} {x1.getModel()} and it was built in {x1.getAn()}");
+
+            Student s = new Student("Viorel Versace", "vx66");
+            s.Domain = "IT";
+            s.prezent();
+
+            BankAccount ba = new BankAccount();
+            ba.Deposit(100);
+            ba.Withdraw(20);
+
+            ba.ShowBalance();
+
+
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+
+            cat.MakeSound();
+            dog.MakeSound();
+
+            Calculator calc = new Calculator();
+
+            Console.WriteLine(calc.Add(5, 10) + " " + calc.Add(3, 11, 4));
+
+            Rectangle rec = new Rectangle();
+            rec.l = 5;
+            rec.L = 10;
+
+            Circle ci = new Circle();
+            ci.r = 3;
+
+            Console.WriteLine( rec.Area());
+            Console.WriteLine(ci.Area());
+
+            CalculatorSalariu cs = new CalculatorSalariu();
+            Console.WriteLine($"Salariul meu anual este de {cs.SalariuAnual(3000)} lei");
+
         }
+
     }
 }
