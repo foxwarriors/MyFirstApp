08bb76160d6fd52dceb002ba4cd6e6d9bc4d17d0
foxwarriors <adi_ciuca@yahoo.com>
2022-02-11 23:34:05 +0200

    Lectia 7

---
+++ Lectia_7/Lectia_7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lectia_7
+{
+
+    public class Parent
+    {
+        public string Name;
+
+
+    }
+
+    public class Child : Parent { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 20;
+            double b = 6.7777;
+
+            a = (int)b;
+
+            Parent p = new Parent() { Name = "Bob" };
+            Child c = new Child() { Name = "Viorel" };
+
+            p = (Parent)c;
+
+
+
+            Console.WriteLine(p.Name);
+            Console.WriteLine(a);
+
+        }
+    }
+}
