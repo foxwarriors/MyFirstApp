d875e7bc3b80ca6ddf41edb291b8a80df554f601
foxwarriors <adi_ciuca@yahoo.com>
2022-02-16 14:45:55 +0200

    Lectia 9 exemplu stream

---
+++ Lectia_9_StreamExemplu/Lectia_9_StreamExemplu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Lectia_9_StreamExemplu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/Users/adrianciuca/Desktop/Hello.txt";
+            using (FileStream writeStream = new FileStream(path, FileMode.Append))
+            {
+                string s = "\nHello Friends!";
+
+                byte[] bytes = Encoding.UTF8.GetBytes(s);
+                writeStream.Write(bytes, 0, bytes.Length);
+            }
+            Console.WriteLine("Finish!");
+
+            Console.ReadLine();
+        }
+    }
+}
