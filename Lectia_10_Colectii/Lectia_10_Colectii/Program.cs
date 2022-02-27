c61bfbb4b74f57ae96f40a7aff1dfe58d9d77c26
foxwarriors <adi_ciuca@yahoo.com>
2022-02-27 23:13:53 +0200

    Introduction to lesson10 lists

---
+++ Lectia_10_Colectii/Lectia_10_Colectii/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lectia_10_Colectii
+{
+    class Program
+    {
+        public void RunLists()
+        {
+            List<string> names = new List<string>();
+            names.Add("Maria");
+            names.Add("Viorel");
+            names.Add("Adi");
+            names.Add("Marius");
+
+            Console.WriteLine(names.Count);
+            Console.WriteLine(names.Capacity);
+
+            for(int i = 0; i < names.Count; i ++)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            program.RunLists();
+        }
+    }
+}
+
