9f393a4857545e2fbb7e62a0bf34ee0fd956a897
foxwarriors <adi_ciuca@yahoo.com>
2022-02-28 10:52:15 +0200

    Added queues

--- Lectia_10_Colectii/Lectia_10_Colectii/Program.cs
+++ Lectia_10_Colectii/Lectia_10_Colectii/Program.cs
@@ -19,6 +19,45 @@ namespace Lectia_10_Colectii
             {
                 Console.WriteLine(names[i]);
             }
+            Console.WriteLine("-----------");
+            names.Remove("Maria");
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine("-----------");
+            List<string> lista = new List<string> { "Luca", "Matei" };
+            names.AddRange(lista);
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+        }
+
+        public void runQueues()
+        {
+            Console.WriteLine("-----------Queues----------");
+            Queue<Person> people = new Queue<Person>();
+            people.Enqueue(new Person { ID = "1", Name = "Giani" });
+            people.Enqueue(new Person { ID = "2", Name = "Veorica" });
+            people.Enqueue(new Person { ID = "3", Name = "Mirel" });
+            people.Enqueue(new Person { ID = "4", Name = "Gigel" });
+
+            Console.WriteLine(people.Dequeue().Name);
+            Console.WriteLine(people.Dequeue().Name);
+            Console.WriteLine(people.Dequeue().Name);
+
+            people.Enqueue(new Person { ID = "5", Name = "Costel" });
+            Console.WriteLine(people.Dequeue().Name);
+            Console.WriteLine(people.Dequeue().Name);
+
+            people.Enqueue(new Person { ID = "6", Name = "Alex" });
+            Console.WriteLine("PEEK: " + people.Peek().Name);
+            Console.WriteLine("DEQUEUE: " + people.Dequeue().Name);
+            Console.WriteLine(people.Count);
+
         }
 
         static void Main(string[] args)
@@ -26,7 +65,14 @@ namespace Lectia_10_Colectii
             Program program = new Program();
 
             program.RunLists();
+
+            program.runQueues();
         }
     }
 }
 
+public class Person
+{
+    public string Name;
+    public string ID;
+}
